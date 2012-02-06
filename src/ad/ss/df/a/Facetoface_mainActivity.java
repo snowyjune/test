06cd5d96564a67760403c65de9611060d9839b5b
snowyjune <claemtis6@naver.com>
2012-02-06 11:44:39 +0900

    commit

---
+++ src/ad/ss/df/a/Facetoface_mainActivity.java
@@ -0,0 +1,38 @@
+package ad.ss.df.a;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class Facetoface_mainActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	TabHost tab;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        tab = (TabHost) findViewById(R.id.tabhost);
+        tab.setup();
+        
+        TabHost.TabSpec spec1 = tab.newTabSpec("friends");
+        TabHost.TabSpec spec2 = tab.newTabSpec("game friends");
+        TabHost.TabSpec spec3 = tab.newTabSpec("invite");
+        
+        spec1.setIndicator("friends");
+        spec2.setIndicator("game firends");
+        spec3.setIndicator("invite");
+        
+        spec1.setContent(R.id.test);
+        spec2.setContent(R.id.test);
+        spec3.setContent(R.id.test);
+        
+        //=3=
+        
+       // tab.addTab(spec1);
+        tab.addTab(spec2);
+        tab.addTab(spec3);
+        
+    }
+}
